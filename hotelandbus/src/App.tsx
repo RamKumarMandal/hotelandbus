d51aafaed3b8cfa41d1a03bcb7a89bab17929fb0
ram <rk19mandal@gmail.com>
2024-01-21 09:08:56 +0545

    second commit

--- hotelandbus/src/App.tsx
+++ hotelandbus/src/App.tsx
@@ -1,51 +1,19 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import React, { useState } from 'react'
+import Home from './pages/Home'
+import Hotels from './pages/Hotels'
+import List from './pages/List'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App" data-testid="app-page">
-      <header className="App-header">
-        <p className="header">
-          🚀 Vite + React + Typescript + React Testing Library 🤘 <br />& Eslint
-          🔥+ Prettier
-        </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p>
-            {' '}
-            Don&apos;t forgot to install Eslint and Prettier in Your Vscode.
-          </p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer">
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/hotel" element={<Hotels />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ hotelandbus/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Nav from '../components/navbar/Nav'
+
+function Home() {
+  return <Nav />
+}
+
+export default Home
